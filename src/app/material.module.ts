4b85eb99926f6635eca4358c28f1f2f50b9b1c76
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-06-22 16:49:39 +0200

    Adding a mat-icon-button

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [MatSidenavModule, MatToolbarModule],
-  exports: [MatSidenavModule, MatToolbarModule],
+  imports: [MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  exports: [MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule],
 })
 export class MaterialModule {}
