923fd1184cc68e1f9f5c3cf135f14460bc0f9a1c
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-06-22 18:31:59 +0200

    Making the mat-sidenav-container responsive

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,22 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  imports: [MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule],
-  exports: [MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+  ],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+  ],
 })
 export class MaterialModule {}
