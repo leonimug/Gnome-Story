2b81beb9d0c3809899a8eb38d6c1528772008562
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-06-24 12:47:25 +0200

    Adding methods

--- src/app/components/gnome-detail/gnome-detail.component.ts
+++ src/app/components/gnome-detail/gnome-detail.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { GnomesService } from 'src/app/shared/gnomes.service';
+import { IGnome } from 'src/app/shared/gnome.model';
 
 @Component({
   selector: 'app-gnome-detail',
   templateUrl: './gnome-detail.component.html',
-  styleUrls: ['./gnome-detail.component.css']
+  styleUrls: ['./gnome-detail.component.css'],
 })
 export class GnomeDetailComponent implements OnInit {
-
-  constructor() { }
+  gnome: IGnome;
+  constructor(
+    private gnomesService: GnomesService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    this.getGnome();
   }
 
+  getGnome(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.gnomesService.getGnome(id).subscribe((gnome) => (this.gnome = gnome));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
